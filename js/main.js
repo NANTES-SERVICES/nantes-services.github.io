c85b0da4ce38cc40626af2860bebbd1227ac7e3f
Ethan <ebeauvaisguibert@gmail.com>
2019-08-21 15:58:25 +0200

    V 0.0.2 (REDIRECTION & INDENTATION)

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ $("<select />").appendTo("nav");
 
 // Create default option "Go to..."
 $("<option />", {
-   "selected": "selected",
-   "value"   : "",
-   "text"    : "Go to..."
+  "selected": "selected",
+  "value"   : "",
+  "text"    : "Go to..."
 }).appendTo("nav select");
 
 // Populate dropdown with menu items
 $("nav a").each(function() {
- var el = $(this);
- $("<option />", {
-     "value"   : el.attr("href"),
-     "text"    : el.text()
- }).appendTo("nav select");
+  var el = $(this);
+  $("<option />", {
+    "value"   : el.attr("href"),
+    "text"    : el.text()
+  }).appendTo("nav select");
 });
 
 $("nav select").change(function() {
@@ -22,29 +22,29 @@ $("nav select").change(function() {
 
 function afficheMenu(obj){
 
-	var idMenu     = obj.id;
-	var idSousMenu = 'sous' + idMenu;
-	var sousMenu   = document.getElementById(idSousMenu);
+  var idMenu     = obj.id;
+  var idSousMenu = 'sous' + idMenu;
+  var sousMenu   = document.getElementById(idSousMenu);
 
-	/*****************************************************/
-	/**	on cache tous les sous-menus pour n'afficher    **/
-	/** que celui dont le menu correspondant est cliqué **/
-	/** où 4 correspond au nombre de sous-menus         **/
-	/*****************************************************/
-	for(var i = 1; i <= 4; i++){
-		if(document.getElementById('sousmenu' + i) && document.getElementById('sousmenu' + i) != sousMenu){
-			document.getElementById('sousmenu' + i).style.display = "none";
-		}
-	}
+  /*****************************************************/
+  /**	on cache tous les sous-menus pour n'afficher    **/
+  /** que celui dont le menu correspondant est cliqué **/
+  /** où 4 correspond au nombre de sous-menus         **/
+  /*****************************************************/
+  for(var i = 1; i <= 4; i++){
+    if(document.getElementById('sousmenu' + i) && document.getElementById('sousmenu' + i) != sousMenu){
+      document.getElementById('sousmenu' + i).style.display = "none";
+    }
+  }
 
-	if(sousMenu){
-		//alert(sousMenu.style.display);
-		if(sousMenu.style.display == "block"){
-			sousMenu.style.display = "none";
-		}
-		else{
-			sousMenu.style.display = "block";
-		}
-	}
+  if(sousMenu){
+    //alert(sousMenu.style.display);
+    if(sousMenu.style.display == "block"){
+      sousMenu.style.display = "none";
+    }
+    else{
+      sousMenu.style.display = "block";
+    }
+  }
 
 }
