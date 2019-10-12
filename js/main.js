f1dfe8567c32a551400b42609446b721ce1df381
Ethan <ebeauvaisguibert@gmail.com>
2019-10-12 21:00:26 +0200

    MAJ page ACCUEIL

--- js/main.js
+++ js/main.js
@@ -21,17 +21,10 @@ $("nav select").change(function() {
 });
 
 function afficheMenu(obj){
-
   var idMenu     = obj.id;
   var idSousMenu = 'sous' + idMenu;
   var sousMenu   = document.getElementById(idSousMenu);
 
-
-  /*****************************************************/
-  /**	on cache tous les sous-menus pour n'afficher    **/
-  /** que celui dont le menu correspondant est cliqué **/
-  /** où 4 correspond au nombre de sous-menus         **/
-  /*****************************************************/
 
   for(var i = 1; i <= 4; i++){
     if(document.getElementById('sousmenu' + i) && document.getElementById('sousmenu' + i) != sousMenu){
@@ -60,8 +53,10 @@ function cacheMain(obj) {
 
   if (sousMenu.style.display == "block") {
     main.style.display = "none";
+    TweenMax.to(".btnmenu", 0.5, {rotation:90})
   }
   else if (sousMenu.style.display == "none") {
     main.style.display = "block";
+    TweenMax.to(".btnmenu", 0.5, {rotation:0})
   }
 }
